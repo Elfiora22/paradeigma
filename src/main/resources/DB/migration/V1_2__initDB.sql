79464fed08b3c0dc89c31569ec853b0b2fef66f7
elena fostachuk <elfiora2@gmail.com>
2017-11-01 15:47:38 +0200

    20.10migration's working right(i hope)

--- src/main/resources/DB/migration/V1_2__initDB.sql
+++ src/main/resources/DB/migration/V1_2__initDB.sql
@@ -1,31 +1,33 @@
 CREATE TABLE  interviewer(
-id int not null,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 phone_number VARCHAR (10),
-login VARCHAR (10),
-password VARCHAR (8),
-PRIMARY KEY (id)
+login VARCHAR (10) UNIQUE,
+password VARCHAR (8)
 );
+
 CREATE TABLE questionnaire(
-id INT  NOT NULL,
-title VARCHAR (256),
-PRIMARY KEY (id)
+id INT NOT NULL PRIMARY KEY,
+title VARCHAR(256)
 );
+
 CREATE TABLE question(
-id INT NOT NULL,
+id INT NOT NULL PRIMARY KEY,
 question_text VARCHAR (256),
 questionnaire_id INT,
 CONSTRAINT questionnaire_fk FOREIGN KEY (questionnaire_id)REFERENCES questionnaire(id)
 );
+
 CREATE TABLE survey_session(
-id INT  NOT NULL,
+id INT NOT NULL UNIQUE  PRIMARY KEY,
 date_time TIMESTAMP,
 interviewer_id INT,
+CONSTRAINT interviewer_id_fk FOREIGN KEY (interviewer_id)REFERENCES interviewer(id),
 questionnaire_id INT,
-CONSTRAINT interviewer_fk FOREIGN KEY (interviewer_id)REFERENCES interviewer(id),
-CONSTRAINT questionnaire_fk FOREIGN KEY (questionnaire_id)REFERENCES questionnaire(id)
+CONSTRAINT questionnaire_id_fk FOREIGN KEY (questionnaire_id)REFERENCES questionnaire(id)
 );
+
 CREATE TABLE scope(
-id INT NOT NULL,
+id INT NOT NULL PRIMARY KEY,
 answer_type VARCHAR (256),
 text VARCHAR (256),
 question_id INT,
@@ -38,7 +40,7 @@ answer_text VARCHAR (256),
 question_id INT,
 scope_id INT,
 survey_session_id INT,
-CONSTRAINT question_fk FOREIGN KEY (question_id)REFERENCES question(id),
-CONSTRAINT scope_fk FOREIGN KEY (scope_id)REFERENCES scope(id),
-CONSTRAINT survey_sesssion_fk FOREIGN KEY (survey_session_id)REFERENCES survey_session(id)
+CONSTRAINT question_id_fk FOREIGN KEY (question_id)REFERENCES question(id),
+CONSTRAINT scope_id_fk FOREIGN KEY (scope_id)REFERENCES scope(id),
+CONSTRAINT survey_session_id_fk FOREIGN KEY (survey_session_id)REFERENCES survey_session(id)
 );
