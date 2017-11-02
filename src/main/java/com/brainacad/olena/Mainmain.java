cc2555f8de86b9493590604533baefab6c791c24
elena fostachuk <elfiora2@gmail.com>
2017-11-02 21:19:17 +0200

    20.10changes not good?

--- src/main/java/com/brainacad/olena/Mainmain.java
+++ src/main/java/com/brainacad/olena/Mainmain.java
@@ -1,10 +1,14 @@
 package com.brainacad.olena;
 
+import com.brainacad.olena.dao.AnswerDao;
+import com.brainacad.olena.entities.Answer;
 import org.flywaydb.core.Flyway;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
 import java.util.Properties;
+
 import static com.brainacad.olena.dao.DbUtils.*;
 
 /**
@@ -21,6 +25,25 @@ public class Mainmain {
         flyway.setDataSource(props.getProperty(URL), props.getProperty(USER), props.getProperty(PASSWORD));
         flyway.migrate();
     }
+
+
+    Answer answer = new Answer();
+    Long id = Long.valueOf(123);
+    String answerText = "There is some life on Mars";
+    Long questionId = Long.valueOf(22);
+    Long scopeId = Long.valueOf(21);
+    Long surveySessionId = Long.valueOf(55);
+
+    public void saveAnswer(Answer answer) throws SQLException {
+        if (id != 0 | answerText != null | questionId != 0 | scopeId != 0 | surveySessionId != 0) {
+            AnswerDao.saveAnswer(answer);
+        } else System.out.println("Save failed");
+    }
 }
+
+
+
+
+
 
 
